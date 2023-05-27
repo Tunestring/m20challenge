e520159426580ead654e6a308ebc3ce8e14ae849
Nate Long <nate.k.long@gmail.com>
2023-05-27 10:36:58 -0400

    added target='_blank' to footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,14 +25,11 @@ function Footer() {
 
   return (
     <div className="d-flex justify-content-center mt-5">
-      <a href="https://www.linkedin.com/in/nate-long-94987a269/">
+      <a href="https://www.linkedin.com/in/nate-long-94987a269/" target="_blank" rel="noreferrer">
         <i className="fa fa-linkedin-square mx-2" style={styles.iconSize}></i>
       </a>
       <a
-        href="https://github.com/Tunestring"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseExit}
-      >
+        href="https://github.com/Tunestring" target="_blank" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit} rel="noreferrer">
         <i className="fa fa-github mx-2" style={iconStyle}></i>
       </a>
     </div>
